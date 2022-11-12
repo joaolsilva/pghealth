76467e15344c387ce4118805b3805534aa33394a
João Silva <joao.silva@r2discover.com>
2022-11-12 11:54:34 +0000

    Drop root privileges

--- main.go
+++ main.go
@@ -3,9 +3,30 @@ package main
 import (
 	"github.com/joaolsilva/pghealth/cmd"
 	"os"
+	"syscall"
 )
 
+func dropRootPrivileges() (err error) {
+	if syscall.Getuid() != 0 {
+		return nil
+	}
+	// Change user / group to nobody (65534)
+	err = syscall.Setgid(65534)
+	if err != nil {
+		return err
+	}
+
+	err = syscall.Setuid(65534)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
+	if err := dropRootPrivileges(); err != nil {
+		panic(err)
+	}
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
